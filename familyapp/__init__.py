8024b706cb7957ebcd60fad17fe88a590ef22c63
arjun <arjunqwerty4703@gmail.com>
2021-07-11 17:37:08 +0530

    secretkey change

--- familyapp/__init__.py
+++ familyapp/__init__.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 env = "prod"
 if env == "dev":
     app.debug = True
-    app.config["SECRET_KEY"] = "familywebsiteissimple"
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Postgre-arj4703@localhost/familydatabase"
+    app.config["SECRET_KEY"] = "familywebsiteissimple"
 else:
     app.debug = False
-    app.config["SECRET_KEY"] = os.environ["secret"]
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://nxsvptdeqefttg:0f049899f41b857f9d99175b915a237fd008a3cedb2b94e731af79684c4c2510@ec2-34-195-143-54.compute-1.amazonaws.com:5432/d70fr7k9if8sim"
+    app.config["SECRET_KEY"] = "nambaennavechalumidhuthappunnudhansollapogudhu"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
